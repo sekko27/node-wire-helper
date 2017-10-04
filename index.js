41a01e404435ca9fd3f8bdd8bb5e7e88caf3b96f
Balazs Kovacs <balazs.kovacs@metrix.co.hu>
2017-10-04 12:40:53 +0200

    Export Aware interface.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     Helper: require('./lib/Helper'),
     Runner: require('./lib/Runner'),
     Loader: require('./lib/loader/CoCModuleLoader'),
+    Aware: require('./lib/plugins/Aware'),
     beans: {
         ConfigurableFactory: require('./lib/beans/ConfigurableFactory')
     }
